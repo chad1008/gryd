fc7c3f75bd6f153547cb5347c46c80e9890e5c63
Chad Chadbourne <chad1008@Chads-MacBook-Pro.local>
2017-01-13 18:09:56 -0500

    grid alterations and hover effects

--- components/post/content-grid.php
+++ components/post/content-grid.php
@@ -11,17 +11,21 @@
 
 <?php $thumbnail_background = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' ); ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>
-         style="background-image:url('<?php echo esc_url( $thumbnail_background['0'] ); ?>')">
-	<div class="title-box">
-		<header class="entry-header">
-			<?php
-			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}
-			?>
-		</header>
-	</div>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<a class="entry-link" href="<?php esc_url( get_permalink() ) ?>" rel="bookmark">
+		<div class="title-box">
+			<header class="entry-header">
+				<?php
+				if ( is_single() ) {
+					the_title( '<h1 class="entry-title">', '</h1>' );
+				} else {
+					the_title( '<h2 class="entry-title">', '</h2>' );
+				}
+				?>
+
+			</header>
+		</div>
+	</a>
+	<div class="entry-thumbnail" style="background-image:url('<?php echo esc_url( $thumbnail_background['0'] ); ?>')"></div>
+
 </article><!-- #post-## -->
