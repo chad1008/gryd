ffb55d620248816cd2ab6e7cf2e2c19ba792748e
Chad Chadbourne <chad1008@Chads-MacBook-Pro.local>
2017-01-13 16:18:32 -0500

    initial grid setup

--- components/post/content-grid.php
+++ components/post/content-grid.php
@@ -9,22 +9,19 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if ( '' != get_the_post_thumbnail() ) : ?>
-		<div class="post-thumbnail">
-			<a href="<?php the_permalink(); ?>">
-				<?php the_post_thumbnail( 'gryd-featured-image' ); ?>
-			</a>
-		</div>
-	<?php endif; ?>
+<?php $thumbnail_background = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' ); ?>
 
-	<header class="entry-header">
-		<?php
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>
+         style="background-image:url('<?php echo esc_url( $thumbnail_background['0'] ); ?>')">
+	<div class="title-box">
+		<header class="entry-header">
+			<?php
 			if ( is_single() ) {
 				the_title( '<h1 class="entry-title">', '</h1>' );
 			} else {
 				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 			}
-		?>
-	</header>
+			?>
+		</header>
+	</div>
 </article><!-- #post-## -->
