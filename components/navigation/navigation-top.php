543b6aff4ec00290c4d83a88acb49844bcea8495
Chad Chadbourne <chad1008@Chads-MacBook-Pro.local>
2017-01-26 21:58:37 -0500

    top nav styling (not done yet!)

--- components/navigation/navigation-top.php
+++ components/navigation/navigation-top.php
@@ -1,4 +1,4 @@
 <nav id="site-navigation" class="main-navigation" role="navigation">
 	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'gryd' ); ?></button>
-	<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'top-menu' ) ); ?>
+	<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'top-menu', 'container_id' => 'top-menu-wrap' ) ); ?>
 </nav><!-- #site-navigation -->
