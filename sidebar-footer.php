a7ceb012b89064bdd4c616115b7bcc06d351b721
Chad Chadbourne <chad1008@Chads-MBP.fios-router.home>
2017-03-15 15:03:27 -0400

    set .wrap width at 1200px

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -13,21 +13,24 @@ if ( ! is_active_sidebar( 'sidebar-2' ) && ! is_active_sidebar( 'sidebar-3' ) &&
 ?>
 
 <div id="tertiary" class="widget-area footer-widget-area" role="complementary">
-	<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
-		<div id="widget-area-2" class="widget-area">
-			<?php dynamic_sidebar( 'sidebar-2' ); ?>
-		</div><!-- #widget-area-2 -->
-	<?php endif; ?>
+	<div class="wrap">
+		<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+			<div id="widget-area-2" class="widget-area">
+				<?php dynamic_sidebar( 'sidebar-2' ); ?>
+			</div><!-- #widget-area-2 -->
+		<?php endif; ?>
 
-	<?php if ( is_active_sidebar( 'sidebar-3' ) ) : ?>
-		<div id="widget-area-3" class="widget-area">
-			<?php dynamic_sidebar( 'sidebar-3' ); ?>
-		</div><!-- #widget-area-3 -->
-	<?php endif; ?>
+		<?php if ( is_active_sidebar( 'sidebar-3' ) ) : ?>
+			<div id="widget-area-3" class="widget-area">
+				<?php dynamic_sidebar( 'sidebar-3' ); ?>
+			</div><!-- #widget-area-3 -->
+		<?php endif; ?>
 
-	<?php if ( is_active_sidebar( 'sidebar-4' ) ) : ?>
-		<div id="widget-area-4" class="widget-area">
-			<?php dynamic_sidebar( 'sidebar-4' ); ?>
-		</div><!-- #widget-area-4 -->
-	<?php endif; ?>
+		<?php if ( is_active_sidebar( 'sidebar-4' ) ) : ?>
+			<div id="widget-area-4" class="widget-area">
+				<?php dynamic_sidebar( 'sidebar-4' ); ?>
+			</div><!-- #widget-area-4 -->
+		<?php endif; ?>
+	</div><!-- .wrap -->
 </div><!-- #tertiary -->
+
