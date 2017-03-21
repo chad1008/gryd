6af76dd53fd33451c42f2d2f49191425d6ee505e
Chad Chadbourne <chad1008@Chads-MBP.fios-router.home>
2017-03-21 14:51:37 -0400

    working JS for post borders

--- assets/js/gryd.js
+++ assets/js/gryd.js
@@ -1,23 +1,38 @@
-( function( $ ) {
+(function ($) {
 
     function boardwalk_colors() {
 
-        $( '.without-featured-image' ).each( function() {
-            // If the previous post had no image, add the narrow class
-            if ( $( this ).prev().hasClass( 'without-featured-image' )  ) {
-                $( this ).addClass( 'NARROW' );
+        var has_thumbs = [];
+
+        $('.hentry').each(function () {
+
+            // Grab the lenght of the array to start
+            var has_thumbs_length = has_thumbs.length;
+
+            // Limit the array to two values
+            if (has_thumbs_length > 2) {
+                has_thumbs.shift()
             }
 
-            // If there was no image two posts ago, add the wide class
-            if ( $( this ).prev().prev().hasClass( 'without-featured-image' ) ) {
-                $( this ).addClass( 'WIDE' );
+            // As long as there are at least two posts, check our array
+            if (has_thumbs_length >= 2) {
+                // If the current and previous post had no image, add the narrow class
+                if ($(this).hasClass('without-featured-image') && has_thumbs[1] === true) {
+                    $(this).addClass('NARROW')
+                }
+                // If current post and the second to last post had no image, add the wide class
+                if ($(this).hasClass('without-featured-image') && has_thumbs[0] === true) {
+                    $(this).addClass('WIDE')
+                }
             }
 
-        } );
+            // Add the current post's featured image status to the array
+            has_thumbs.push($(this).hasClass('without-featured-image'))
+        });
     }
 
-    $( window ).load( boardwalk_colors );
+    $(window).load(boardwalk_colors);
 
-    $( document ).on( 'post-load', boardwalk_colors );
+    $(document).on('post-load', boardwalk_colors);
 
-} )( jQuery );
+})(jQuery);
